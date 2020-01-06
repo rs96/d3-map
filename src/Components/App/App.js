8da8a6561b553e13e7bb8cf40e8f4f401da7e427
rs96 <shipleymrobert@gmail.com>
2020-01-06 17:17:55 +0000

    Initial folder structuring

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Map from "../Map/Map";
+import logo from "../../Images/logo.svg";
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <Map />
+  </div>
+);
+
+export default App;
---
+++ src/Components/Map/Map.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Map = () => <div>This is the map</div>;
+
+export default Map;
