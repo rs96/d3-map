6378ff08b385d7cf133f8da5a5b5df2c2bed1b9d
rs96 <shipleymrobert@gmail.com>
2020-01-07 17:04:37 +0000

    Initial changes

---
+++ src/Components/DetailsLoadout/DetailsLoadout.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+
+import "./DetailsLoadout.scss";
+
+const DetailsLoadout = () => {
+  useEffect(() => {});
+
+  return (
+    <div className="locationDetailsLoadout">
+      <div className="locationName">Location Name</div>
+      <div>Image</div>
+      <div className="locationDetails">Other details</div>
+    </div>
+  );
+};
+
+export default DetailsLoadout;
